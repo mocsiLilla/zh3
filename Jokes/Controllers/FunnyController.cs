89c3450caec47e1b5166be144ec561d366bc44ad
mocsi <mocsi@LAPTOP-VH3VH20M>
2022-12-12 23:31:43 +0100

    Működik

---
+++ Jokes/Controllers/FunnyController.cs
@@ -0,0 +1,83 @@
+﻿using Jokes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Jokes.Models;
+
+namespace Jokes.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class FunnyController : ControllerBase
+    {
+        [HttpGet]
+        [Route("jokes/all")]
+        public IActionResult M1()
+        {
+            FunnyContext context = new FunnyContext();
+            var viccek = from x in context.Jokes
+                         select x;
+            return Ok(viccek);
+        }
+
+        [HttpGet]
+        [Route("jokes/{id}")]
+        public IActionResult M2(int id)
+        {
+            FunnyContext context = new FunnyContext();
+            var választottVicc = (from x in context.Jokes
+                                 where x.JokeSk == id
+                                 select x).FirstOrDefault();
+            if (választottVicc == null) return BadRequest("Nincs ilyen sorszámú vicc");
+            return Ok(választottVicc);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public void Delete(int id)
+        {
+            FunnyContext context = new FunnyContext();
+            var törlendő = (from x in context.Jokes
+                           where x.JokeSk == id
+                           select x).FirstOrDefault();
+            context.Remove(törlendő);
+            context.SaveChanges();
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public void Add(string vicc)
+        {
+            FunnyContext context = new FunnyContext();
+            Joke ujVicc = new Joke();
+            ujVicc.JokeText = vicc;
+            context.Jokes.Add(ujVicc);
+            context.SaveChanges();
+        }
+
+        [HttpPost]
+        [Route("like/{id}")]
+        public void Like(int id)
+        {
+            FunnyContext context = new FunnyContext();
+            var jóVicc = (from x in context.Jokes
+                          where x.JokeSk == id
+                          select x).FirstOrDefault();
+            jóVicc.UpVotes++;
+            context.Update(jóVicc);
+            context.SaveChanges();
+        }
+
+        [HttpPost]
+        [Route("dislike/{id}")]
+        public void Dislike(int id)
+        {
+            FunnyContext context = new FunnyContext();
+            var jóVicc = (from x in context.Jokes
+                          where x.JokeSk == id
+                          select x).FirstOrDefault();
+            jóVicc.DownVotes++;
+            context.Update(jóVicc);
+            context.SaveChanges();
+        }
+    }
+}
--- Jokes/wwwroot/main.js
+++ Jokes/wwwroot/main.js
@@ -1,1 +1,34 @@
-﻿
+﻿fetch(`jokes/all`)
+    .then(response => response.json())
+    .then(data => IdListába(data));
+
+var jokeSk = [];
+var sorszám = 0;
+
+function IdListába(data) {
+    for (var i = 0; i < data.length; i++) {
+        var szám = data[i].jokeSk;
+        jokeSk.push(szám);
+    }
+    ViccBetöltés(jokeSk[sorszám]);
+}
+
+function ViccBetöltés(id) {
+    fetch(`jokes/${id}`)
+        .then(response => response.json())
+        .then(data => Kiíratás(data));
+}
+
+function Kiíratás(data) {
+    let elem = data.jokeText;
+    document.getElementById("választottVicc").innerText = elem;
+}
+
+function KövetkezőVicc() {
+    sorszám++;
+    ViccBetöltés(jokeSk[sorszám]);
+}
+
+window.onload = function(e) {
+    document.getElementById("add").onclick = KövetkezőVicc;
+}
